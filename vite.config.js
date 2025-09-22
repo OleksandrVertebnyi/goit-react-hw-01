711fd55f6f3d82dfd4a767fa698e350500abc578
Oleksandr <sash1983direct@gmail.com>
2025-09-21 19:35:01 -0700

    Completed homework

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-export default defineConfig({
-  base: '/goit-react-hw-01/',
-  plugins: [react()],
-})
+const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES'
 
+export default defineConfig({
+  plugins: [react()],
+  base: isGitHubPages ? '/goit-react-hw-01/' : '/',
+})
